c29276f2fea21dc5e235d356aa998449d1fe0736
Jerome Robert <jeromerobert@gmx.com>
2016-05-10 10:50:06 +0200

    Allow to create alternative UncompressedValues implementations

--- src/uncompressed_values.hpp
+++ src/uncompressed_values.hpp
@@ -22,15 +22,15 @@
 
 #include "h_matrix.hpp"
 #include "cluster_tree.hpp"
-#include "rk_matrix.hpp"
 #include <limits>
+#include <algorithm>
 
 namespace hmat {
-/**
- * Extract an uncompressed set of values from the matrix
- */
-template <typename T> class UncompressedValues {
-    const HMatrix<T> &matrix_;
+
+
+template <typename T, template <typename> class M, typename I> class UncompressedValuesBase {
+  protected:
+    const M<T> * matrix_;
     T *values_;
     int valuesLd_;
     typedef std::vector<std::pair<int, int> >::iterator IndiceIt;
@@ -39,51 +39,6 @@ template <typename T> class UncompressedValues {
      * The second element of the pair is the position of the element in the original query.
      */
     IndiceIt rowStart_, rowEnd_, colStart_, colEnd_;
-
-    void getValue(IndiceIt r, IndiceIt c, T v) {
-        values_[r->second + ((size_t)valuesLd_) * c->second] = v;
-    }
-
-    void getNullValues() {
-        for(IndiceIt r = rowStart_; r != rowEnd_; ++r) {
-            for(IndiceIt c = colStart_; c != colEnd_; ++c) {
-                getValue(r, c, Constants<T>::zero);
-            }
-        }
-    }
-
-    void getFullValues() {
-        // Not supported yet
-        assert(matrix_.full()->pivots == NULL);
-        assert(matrix_.full()->diagonal == NULL);
-        int ro = matrix_.rows()->offset();
-        int co = matrix_.cols()->offset();
-        for(IndiceIt r = rowStart_; r != rowEnd_; ++r) {
-            for(IndiceIt c = colStart_; c != colEnd_; ++c) {
-                getValue(r, c, matrix_.full()->get(r->first - ro, c->first - co));
-            }
-        }
-    }
-
-    void getRkValues();
-
-    void getValues() {
-        if (rowStart_ == rowEnd_ || colStart_ == colEnd_)
-            return;
-        if (!matrix_.isLeaf()) {
-            for (int i = 0; i < matrix_.nrChild(); i++) {
-                UncompressedValues view(*this, *matrix_.getChild(i));
-            }
-        } else if (matrix_.isNull()) {
-            getNullValues();
-        } else if (matrix_.isRkMatrix()) {
-            getRkValues();
-        } else if (matrix_.isFullMatrix()) {
-            getFullValues();
-        } else {
-            assert(false);
-        }
-    }
 
     void compatibleQuery(const IndexSet & clusterData, IndiceIt & begin, IndiceIt & end) {
         int lb = clusterData.offset();
@@ -119,32 +74,83 @@ template <typename T> class UncompressedValues {
         std::sort(indices.begin(), indices.end());
     }
 
-    UncompressedValues(const UncompressedValues & o, const HMatrix<T> &matrix)
-        : matrix_(matrix), values_(o.values_), valuesLd_(o.valuesLd_),
-          rowStart_(o.rowStart_), rowEnd_(o.rowEnd_),
-          colStart_(o.colStart_), colEnd_(o.colEnd_)
-    {
-        compatibleQuery(*matrix_.rows(), rowStart_, rowEnd_);
-        compatibleQuery(*matrix_.cols(), colStart_, colEnd_);
-        getValues();
+    void getValues() {
+        if (rowStart_ == rowEnd_ || colStart_ == colEnd_)
+            return;
+        if (!matrix_->isLeaf()) {
+            for (int i = 0; i < matrix_->nrChild(); i++) {
+                I view;
+                M<T> * child = matrix_->getChild(i);
+                view.matrix_ = child;
+                view.values_ = values_;
+                view.valuesLd_ = valuesLd_;
+                view.compatibleQuery(*child->rows(), rowStart_, rowEnd_);
+                view.compatibleQuery(*child->cols(), colStart_, colEnd_);
+                view.getValues();
+            }
+        } else
+            static_cast<I*>(this)->getLeafValues();
     }
-
   public:
-
-    UncompressedValues(const HMatrix<T> &matrix, int * rows, int rowSize, int * cols, int colSize, T *values, int ld = -1)
-        : matrix_(matrix), values_(values), valuesLd_(ld)
+    /** Actually uncompress */
+    void uncompress(const M<T> * matrix, int * rows, int rowSize, int * cols, int colSize, T *values, int ld = -1)
     {
-        assert(matrix.father == NULL);
-        if(valuesLd_ == -1)
-            valuesLd_ = rowSize;
+        assert(matrix->father == NULL);
+        matrix_ = matrix;
+        values_ = values;
+        valuesLd_ = ld == -1 ? rowSize : ld;
         std::vector<std::pair<int, int> > rowsIndices, colsIndices;
-        createQuery(*matrix.rows(), rows, rowSize, rowsIndices);
+        createQuery(*matrix->rows(), rows, rowSize, rowsIndices);
         rowStart_ = rowsIndices.begin();
         rowEnd_ = rowsIndices.end();
-        createQuery(*matrix.cols(), cols, colSize, colsIndices);
+        createQuery(*matrix->cols(), cols, colSize, colsIndices);
         colStart_ = colsIndices.begin();
         colEnd_ = colsIndices.end();
         getValues();
+    }
+};
+
+template <typename T> class UncompressedValues: public UncompressedValuesBase<T, HMatrix, UncompressedValues<T> > {
+    typedef std::vector<std::pair<int, int> >::iterator IndiceIt;
+    friend class UncompressedValuesBase<T, HMatrix, UncompressedValues<T> >;
+    void getValue(IndiceIt r, IndiceIt c, T v) {
+        this->values_[r->second + ((size_t)this->valuesLd_) * c->second] = v;
+    }
+
+    void getNullValues() {
+        for(IndiceIt r = this->rowStart_; r != this->rowEnd_; ++r) {
+            for(IndiceIt c = this->colStart_; c != this->colEnd_; ++c) {
+                getValue(r, c, Constants<T>::zero);
+            }
+        }
+    }
+
+    void getFullValues() {
+        const HMatrix<T> & m = *this->matrix_;
+        // Check for not supported cases
+        assert(m.full()->pivots == NULL);
+        assert(m.full()->diagonal == NULL);
+        int ro = m.rows()->offset();
+        int co = m.cols()->offset();
+        for(IndiceIt r = this->rowStart_; r != this->rowEnd_; ++r) {
+            for(IndiceIt c = this->colStart_; c != this->colEnd_; ++c) {
+                getValue(r, c, m.full()->get(r->first - ro, c->first - co));
+            }
+        }
+    }
+
+    void getRkValues();
+
+    void getLeafValues() {
+        if (this->matrix_->isNull()) {
+            getNullValues();
+        } else if (this->matrix_->isRkMatrix()) {
+            getRkValues();
+        } else if (this->matrix_->isFullMatrix()) {
+            getFullValues();
+        } else {
+            assert(false);
+        }
     }
 };
 }
